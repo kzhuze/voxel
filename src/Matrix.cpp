7c0fb04aa0a05ba639624a41b92236125f05c7ce
Kirill Zhuze <megadymo@gmail.com>
2019-12-24 02:27:32 -0800

    more matrix changes

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -2,79 +2,80 @@
 #include <iostream>
 
 class Matrix {
-	private:
-		float *values;
-		int rows;
-		int cols;
+    private:
+        float *values;
+        int rows;
+        int cols;
 	
-	public:
-		Matrix(int R, int C, int val) {
-			values = new float[R * C];
-			rows = R;
-			cols = C;
-			for (int r=0; r<rows; r++) {
-				for (int c=0; c<cols; c++) {
-					if (r == c) {
-						values[r*rows+c] = 1;
-					} else {
-						values[r*rows+c] = 0;
-					}
-				}
-			}
-		}
-		~Matrix() {
-			delete [] values;
-		}
-		float& operator()(int r, int c) {
-			if (1 <= r && r <= rows) {
-				if (1 <= c && c <= cols) {
-					return values[(r-1)*rows+(c-1)];
-				}
-			}
-			throw std::out_of_range("Matrix indices out of range");
-		}
-		Matrix operator+(Matrix& rhs) {
-			if (this->rows==rhs.rows && this->cols==rhs.cols) {	
-				Matrix result(this->rows, this->cols);
-				for (int r=1; r<=this->rows; r++) {
-					for (int c=1; c<=this->cols; c++) {
-						result(r,c)=(*this)(r,c)+rhs(r,c);
-					}
-				}
-				return result;
-			}
-			throw std::domain_error("Matrix dimensions incompatible");
-		}
-		Matrix operator*(Matrix& rhs) {
-			if (this->cols==rhs.rows) {
-				Matrix result(this->rows, rhs.cols);
-				for (int r=1; r<=this->rows; r++) {
-					for (int c=1; c<=rhs.cols; c++) {
-						for (int k=1; k<=this->cols; k++) {
-							result(r,c) += (*this)(r,k)*rhs(k,c);
-						}
-					}
-				}
-				return result;
-			}
-			throw std::domain_error("Matrix dimensions incompatible");
-		}
-		friend std::ostream& operator<<(std::ostream& os, Matrix& mat) {
-			for (int r=0; r<mat.rows; r++) {
-				for (int c=0; c<mat.cols; c++) {
-					os << mat.values[r*mat.rows+c] << " ";
-				}
-				os << "\n";
-			}
-			return os;
-		}
+    public:
+        Matrix(int _rows, int _cols, float val=1.0) {
+            rows = _rows;
+            cols = _cols;
+            values = new float[rows * cols]();
+            for (int r=0; r<rows; r++) {
+                for (int c=0; c<cols; c++) {
+                    if (r == c) {
+                        values[r*rows+c] = val;
+                    }
+                }
+            }
+        }
+        ~Matrix() {
+            delete [] values;
+        }
+        float& operator()(int r, int c) {
+            if (1 <= r && r <= rows) {
+                if (1 <= c && c <= cols) {
+                    return values[(r-1)*rows+(c-1)];
+                }
+            }
+            throw std::out_of_range("Matrix indices out of range");
+        }
+        Matrix operator+(Matrix& rhs) {
+            if (this->rows==rhs.rows && this->cols==rhs.cols) {	
+                Matrix result(this->rows, this->cols, 0);
+                for (int r=1; r<=this->rows; r++) {
+                    for (int c=1; c<=this->cols; c++) {
+                        result(r,c)=(*this)(r,c)+rhs(r,c);
+                    }
+                }
+                return result;
+            }
+            throw std::domain_error("Matrix dimensions incompatible");
+        }
+        Matrix operator*(Matrix& rhs) {
+            if (this->cols==rhs.rows) {
+                Matrix result(this->rows, rhs.cols, 0);
+                for (int r=1; r<=this->rows; r++) {
+                    for (int c=1; c<=rhs.cols; c++) {
+                        for (int k=1; k<=this->cols; k++) {
+                            result(r,c) += (*this)(r,k)*rhs(k,c);
+                        }
+    
+                    }
+                }
+                return result;
+            }
+            throw std::domain_error("Matrix dimensions incompatible");
+        }
+        friend Matrix operator*(float lhs, Matrix& rhs) {
+            return Matrix(rhs.rows, rhs.cols, lhs)*rhs;
+        }
+        friend std::ostream& operator<<(std::ostream& os, Matrix& rhs) {
+            for (int r=1; r<=rhs.rows; r++) {
+                for (int c=1; c<=rhs.cols; c++) {
+                    os << rhs(r,c) << " ";
+                }
+                os << "\n";
+            }
+            return os;
+        }
 };
 
 int main() {
-	Matrix mat1(2,2);
-	Matrix mat2(2,2);
-	Matrix mat3 = mat1 + mat2;
-	Matrix mat4 = mat3 * mat3;
-	std::cout << mat3 << mat4;
-	return 0;
-}
+    Matrix mat1(3,3,2);
+    mat1(2,1) = 1;
+    std::cout << mat1;
+    //std::cout << 3 * mat1 << mat1 + mat1 << mat1 * mat1;
+    return 0;
+};
